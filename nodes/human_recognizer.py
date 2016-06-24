a9f942d386426a53e239a70d55139b61437af951
demulab-pc2 <demuranet@gmail.com>
2016-06-25 03:39:56 +0900

    tuned parameter

--- nodes/human_recognizer.py
+++ nodes/human_recognizer.py
@@ -24,52 +24,69 @@ class HumanDetector:
         ), cluster.info)
         human_prob = map(lambda prob : prob.all, cluster.prob) 
         thresh = 1.0
-        clusters = hcluster.fclusterdata(human_cluster,
-                                         thresh,
-                                         criterion="distance")
-        cluster_point = np.asarray([0.0 for i in range(len(cluster.candidates))])
+        if len(cluster.candidates) > 0:
+            try:
+                clusters = hcluster.fclusterdata(human_cluster,
+                                                 thresh,
+                                                 criterion="distance")
 
-        for i in range(len(clusters)):
-            if human_prob[i] > 0.85:
-                cluster_point[clusters[i]-1] += float(len(clusters))
-            if human_prob[i] > 0.7:
-                cluster_point[clusters[i]-1] += human_prob[i]
+            except ValueError:
+                self.pos.x = 0.0
+                self.pos.y = 0.0
+                self.pos.z = 0.0
+                self.pos_pub.publish(self.pos)
+                
+            cluster_point = np.asarray([0.0 for i in range(len(cluster.candidates))])
 
-        centroid  = np.zeros((1,3))
-        num = 0
+            for i in range(len(clusters)):
+                if human_prob[i] > 0.85:
+                    cluster_point[clusters[i]-1] += float(len(clusters))
+                if human_prob[i] > 0.85:
+                    cluster_point[clusters[i]-1] += human_prob[i]
+
+            centroid  = np.zeros((1,3))
+            num = 0
     
-        for i in range(len(clusters)):
-            if clusters[i]-1 == np.argmax(cluster_point):
-                centroid += human_cluster[i]
-                num += 1
+            for i in range(len(clusters)):
+                if clusters[i]-1 == np.argmax(cluster_point):
+                    centroid += human_cluster[i]
+                    num += 1
                 if human_prob[i] > 0.5:
                     print human_prob[i]
-        if num > 0:
-            centroid =  centroid / num
-            marker = Marker()
-            marker.header.frame_id = "realsense_frame"
-            marker.action = marker.ADD
-            marker.type = marker.SPHERE
-            marker.color.a = 1.0
-            marker.color.r = 0.0
-            marker.color.g = 1.0
-            marker.color.b = 1.0
-            marker.scale.x = 0.5
-            marker.scale.y = 0.5
-            marker.scale.z = 0.5
-            marker.pose.position.x = centroid[0][0]
-            marker.pose.position.y = centroid[0][1]
-            marker.pose.position.z = centroid[0][2]
-            self.marker_pub.publish(marker)
-            self.pos.x = centroid[0][0]
-            self.pos.y = centroid[0][1]
-            self.pos.z = centroid[0][2]
-            self.pos_pub.publish(pos)
+
+
+            if num > 0:
+                centroid =  centroid / num
+                marker = Marker()
+                marker.header.frame_id = "realsense_frame"
+                marker.action = marker.ADD
+                marker.type = marker.SPHERE
+                marker.color.a = 1.0
+                marker.color.r = 0.0
+                marker.color.g = 1.0
+                marker.color.b = 1.0
+                marker.scale.x = 0.5
+                marker.scale.y = 0.5
+                marker.scale.z = 0.5
+                marker.pose.position.x = centroid[0][0]
+                marker.pose.position.y = centroid[0][1]
+                marker.pose.position.z = centroid[0][2]
+                self.marker_pub.publish(marker)
+                self.pos.x = centroid[0][0]
+                self.pos.y = centroid[0][1]
+                self.pos.z = centroid[0][2]
+                self.pos_pub.publish(self.pos)
+
+            else:
+                rospy.loginfo("no cluster found")
+                self.pos.z = 0.0
+                self.pos_pub.publish(self.pos)
 
         else:
-            rospy.loginfo("no cluster found")
+            self.pos.x = 0.0
+            self.pos.y = 0.0
             self.pos.z = 0.0
-            self.pos_pub.publish(pos)
+            self.pos_pub.publish(self.pos)
         
 
 if __name__ == "__main__":
